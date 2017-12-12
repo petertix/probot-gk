cab8d12018d11a47410bff6e618995fc9c846eb4
Peter Robinson <peter.timothy.robinson@gmail.com>
2017-12-12 10:52:38 -0700

    Shortened index.js  - Added devel files
    Signed-off-by: Peter Robinson <peter.timothy.robinson@gmail.com>

--- index.js
+++ index.js
@@ -1,88 +1,66 @@
-
 const pWaitFor = require('p-wait-for');
-//import pWaitFor from 'p-wait-for';
 
 module.exports = robot => {
+  console.log('The probot App was loaded!');
 
-  console.log("The probot App was loaded!");
+  robot.on('pull_request.opened', async c =>
+    handlePrOpened(c).catch(e => {
+      if (e.code === 405) {
+        const message = JSON.parse(e.message).message;
+        c.payload.issues.createComment(
+          context.issue({
+            body: `Failed to merge PR: ${message}`
+          })
+        );
+      }
+    })
+  );
 
-  robot.on('pull_request.opened', handlePrOpened);
   robot.on('pull_request.labeled', handleLabeled);
 
-  async function handleLabeled (context) {
+  async function handleLabeled(context) {
     const { payload } = context;
 
-    console.log("The issue was Labeled");
-    console.log(payload);
-    
+    console.log('The issue was Labeled');
+
     return;
   }
 
-  async function handlePrOpened (context) {
+  async function handlePrOpened(context) {
     const { payload } = context;
-    const {github} = context;
-    const {issue} = context.payload;
-    
-    // << Debug Start >>
-    // ------------------------------------------------------------------------
-    //console.log(context);
-    console.log("Hey, a PR was Opened");
-    console.log("payload.number: " + payload.number);
-    console.log("payload.pull_request.state: " + payload.pull_request.state);
-    console.log("payload.pull_request.title: " + payload.pull_request.title);
-    console.log("payload.pull_request.user.login: " + payload.pull_request.user.login);
-    console.log("payload.pull_request.user.type: " + payload.pull_request.user.type);
-    // console.log("payload.pull_request.body: " + payload.pull_request.body);
-    console.log("payload.pull_request.statuses_url: " + payload.pull_request.statuses_url);
-    console.log("payload.pull_request.head.ref: " + payload.pull_request.head.ref);
-    console.log("payload.pull_request.head.sha: " + payload.pull_request.head.sha);
-    console.log("payload.pull_request.merged: " + payload.pull_request.merged);
-    console.log("payload.sender.login: " + payload.sender.login);
-    console.log("payload.sender.type: " + payload.sender.type);
-    console.log("owner:" + payload.sender.login);
-    console.log("repo: " + payload.repository.name);
-    console.log("ref: " + payload.pull_request.head.ref);
-    
-    // Test fetching the current status of the PR
-    const theStatus = await github.repos.getCombinedStatusForRef(
-      context.repo({
-        // owner: payload.sender.login,
-        repo: payload.repository.name,
-        ref: payload.pull_request.head.sha,
-        // ref: payload.pull_request.head.ref,
-      }),
-    );
-    
-    //console.log("theStatus: " + theStatus);
-    // console.log(theStatus);
-    
-    console.log("state: " + theStatus.data.state);
-    console.log("sha: " + theStatus.data.sha);
-    console.log("description: " + theStatus.data.repository.description);
-    
-    // console.log(theStatus.data.statuses);
-    
-    // ------------------------------------------------------------------------
-    // << Debug End >>
-    
-    // Fetch the User's permissions 
+    const { github } = context;
+
+    // If the state of the PR is not open, then return
+    if (payload.pull_request.state !== 'open') {
+      return;
+    }
+
+    // If the sender of the PR is not greenkeeper, then return
+    if (payload.pull_request.user.login !== 'greenkeeper[bot]') {
+      return;
+    }
+
+    // Fetch the User's permissions
+    // The probot app user needs to have admin permission
     const permissions = await github.repos.reviewUserPermissionLevel(
       context.repo({
-        username: payload.sender.login,
-        // username: user.login,
-      }),
+        username: payload.pull_request.user.login
+        // username: user.login
+      })
     );
 
     // console.log(permissions);
-    
+
     // If the User's permissions can not perform the merge, then return
     const level = permissions.data.permission;
-    console.log("permissions.data.permission: " + permissions.data.permission);
+    console.log('permissions.data.permission: ' + permissions.data.permission);
 
-    if (level !== 'admin') { // Check write permission, also.
+    if (level !== 'admin') {
+      // Check write permission, also.
       console.log(">>> The User's permissions are not at level: admin. <<<");
-      console.log('>>> This User can not perform a Merge <<<');
-      console.log('User: ' + payload.sender.login);
+      console.log(
+        '>>> The User:' + context.host + ' can not perform a Merge <<<'
+      );
       // return;
     }
 
@@ -93,95 +71,56 @@ module.exports = robot => {
     let repoName = payload.repository.name;
     let refSha = payload.pull_request.head.sha;
 
-    pWaitFor(() => fetchPrState(repoName,refSha),5000).then(() => {
-      console.log('The status is no longer pending');
-    })
+    await pWaitFor(() => fetchPrState(repoName, refSha), 5000);
 
+    console.log('Now, the merge...');
+    console.log('number: ' + payload.pull_request.number);
+    console.log('commit_title: ' + payload.pull_request.title);
+    console.log('commit_message: ' + 'Auto merge of a green-keeper PR');
+    console.log('\n...Still Testing...Merge did NOT Occur...\n');
+
+    const message = 'This is an Automated message';
+    github.issues.createComment(
+      context.issue({
+        body: `Attempting to merge PR: ${message}`
+      })
+    );
+
+    //    return; // For Testing
+
+    const { merge_method } = {
+      merge_method: 'merge'
+    };
+
+    // Attempt to merge the Pull Request
+    // Try to perform the merge with the issue Number, title,
+    // commit message and the merge method.
+    await github.pullRequests.merge(
+      context.repo({
+        number: payload.pull_request.number,
+        commit_title: payload.pull_request.title,
+        commit_message: payload.pull_request.html_url,
+        merge_method
+      })
+    );
 
     // The fetchPrState function is polled repeatedly until the
     // PR state changes to a non-pending state.
     // If the state is: success, then attempt to merge the PR.
     // If the state is: failure or non-pending, then return.
     //
-    async function fetchPrState (repoName, refSha) {
-      let stopPolling = false;
-
+    async function fetchPrState(repoName, refSha) {
       // Fetch the combined status of the PR from all CI test results.
       const prStatus = await github.repos.getCombinedStatusForRef(
         context.repo({
           repo: repoName,
-          ref: refSha,
-        }),
+          ref: refSha
+        })
       );
 
-      console.log('Pull Request State: ' + prStatus.data.state);
+      console.log('State: ' + prStatus.data.state);
 
-      // Check if the state is no longer in the pending state
-      if (prStatus.data.state === 'failure') {
-        stopPolling = true;
-        console.log('>>> One or more CI tests have failed. <<<');
-        console.log("The PR State is now: " +  prStatus.data.state);
-        console.log('The PR will NOT be Merged.');
-      }
-      else if (prStatus.data.state === 'success') {
-        
-        stopPolling = true;
-        console.log("The PR State is now: " +  prStatus.data.state);
-        console.log('>>> The CI tests have Passed. <<<');
-        console.log('The PR will be Merged now...skipping while Testing');
-
-        // Attempt to merge the Pull Request
-        // Try to perform the merge with the issue Number, title, 
-        // commit message and the merge method.
-        try {
-          await github.pullRequests.merge(
-            context.repo({
-              number: issue.number,
-              commit_title: issue.title,
-              commit_message: issue.html_url,
-              merge_method,
-            }),
-          );
-
-          console.log('Made it 10!');
-
-        } catch (err) {
-          console.log(err.code);
-          
-          if (err.code === 405) {
-            const message = JSON.parse(err.message).message;
-            github.issues.createComment(
-              context.issue({
-                body: `Failed to merge PR: ${message}`,
-              }),
-            );
-          }
-
-          // A 403 suggests that the github App's permissions and webhooks need addressing
-          else if (err.code === 403) {
-            const message = JSON.parse(err.message).message;
-            console.log(message);
-
-          //  github.issues.createComment(
-          //    context.issue({
-          //      body: `Failed to merge PR: ${message}`,
-          //    }),
-          //  );
-          }
-
-          console.log('Made it 20');
-        }
-      }
-      else if (prStatus.data.state !== 'pending') {
-        stopPolling = true;
-        console.log('>>> Invalid State. The PR state is not success|failure|polling <<<');
-        console.log("The PR State is now: " +  prStatus.data.state);
-        console.log('The PR will NOT be Merged.');
-      }
-      
-      return stopPolling;
+      return prStatus.data.state === 'success';
     }
-
-    return;
   }
 };
